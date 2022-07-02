55ca9323404742a3a985acbbce4a67a0a5b45834
rasheed <dimexylora@gmail.com>
2022-07-02 05:51:55 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,9 @@ int main(void)
 				putchar((digit3 % 10) + '0');
 
 			if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-			continue;									         putchar(',');									 putchar(' ');
+			continue;
+				putchar(',')
+				putchar(',');
 			}
 		}
 
