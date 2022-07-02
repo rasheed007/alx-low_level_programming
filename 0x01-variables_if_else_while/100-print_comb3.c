face0a6e1b229ce919dad3f3b2736da92fd41316
rasheed <dimexylora@gmail.com>
2022-07-02 05:11:01 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,31 +7,23 @@
 int main(void)
 {
 	int tens;
-	
 	int ones;
-	
-	for (tens = 0; tens <= 9; tens++)
-		{
-			for (ones = tens + 1; ones <= 9; ones++)
+
+		for (tens = 0; tens <= 9; tens++)
 			{
-				putchar(tens + '0');
-				
-				putchar(ones + '0');
-
-
-
-				if (tens < 8)
-
-					{
-						putchar(',');
-						
+				for (ones = tens + 1; ones <= 9; ones++)
+					{			
+					putchar(tens + '0');
+					putchar(ones + '0');
+					
+					if (tens < 8)
+						{	
+						putchar(',');					
 						putchar(' ');
-					}								}
+						}									}
+			}	
 
-		}
-
-	        putchar('\n');
-
-		return (0);
+	putchar('\n');
+	return (0);
 
 }
