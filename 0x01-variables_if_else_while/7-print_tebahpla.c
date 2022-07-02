e3643b89ac38b3965de20b9bd9e16b9e9be02862
rasheed <dimexylora@gmail.com>
2022-07-02 00:52:27 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,11 +5,19 @@
  */
 int main(void)
 {
+
 	char letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
 	putchar(letter);
-	putchar('\n'):
-	
-	return (0):
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
 }
+
+
