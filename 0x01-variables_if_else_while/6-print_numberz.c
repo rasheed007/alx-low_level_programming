d4b0f78c64474f501fb92c3a86dbeebf41ac0e67
rasheed <dimexylora@gmail.com>
2022-07-02 02:10:08 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,20 +6,14 @@
  */
 int main(void)
 {
+	int num;
 
-	 int num;
+	for (num =0; num < 19; num ++)
+		putchar((num % 10) + '0');
 
-
-
-	 for (num = 0; num < 10; num++)
-
-		    putchar((num % 10) + '0');
+	putchar('\n');
 
 
-
-	  putchar('\n');
-
-
-	  return (0);
+	return (0);
 
 }
