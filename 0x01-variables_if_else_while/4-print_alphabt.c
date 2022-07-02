dbac457f4d41a2e871dd55afd1e32ac57c9dec6d
rasheed <dimexylora@gmail.com>
2022-07-02 03:01:53 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,16 @@
 int main(void)
 
 {
-	char letter;	
-	
+	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
-	
-	{if (letter != 'e' && letter != 'q')putchar(letter);
+
+	{
+		if (letter != 'e' && letter != 'q')putchar(letter);
 
 		}
 
-
 	putchar('\n');
-	
 	return (0);
 
 }
