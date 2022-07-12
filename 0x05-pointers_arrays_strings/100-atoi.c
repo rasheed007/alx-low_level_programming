43cb1790762cb6753a4d33c2b1b3c307cd019842
rasheed <dimexylora@gmail.com>
2022-07-12 15:20:29 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,6 +17,12 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
 	{
 		if (s[i] == '-')
+			sign = sign * -1;
+	}
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
 			val = val * 10 + sign * (s[i] - '0');
 		if (val != 0 && !(s[i] >= '0' && s[i] <= '9'))
 			return (val);
