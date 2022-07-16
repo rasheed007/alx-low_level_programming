19813fc9528bd45886d39d043bb83403eb522d72
rasheed <dimexylora@gmail.com>
2022-07-15 18:01:33 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main -> main function
+ * Return: 0 success
+ */
 int main(void)
 {
 	int n;
@@ -8,13 +12,9 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
+	/**
 	 * write your line of code here...
 	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
 	 */
 	p[5] = 98;
 	/* ...so that this prints 98\n */
